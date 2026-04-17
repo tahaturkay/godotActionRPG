4756c579fcf8039ee1446e50711834c9187c8586
Taha Turkay <tahaturkay01@gmail.com>
2026-04-17 22:04:32 +0300

    Enemy line of sight

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
 const speed = 30.0
 
-@export var range: = 512
+@export var range: = 128
 
 @onready var animation_tree: AnimationTree = $AnimationTree # Accessing animationTree node
 @onready var sprite_2d: Sprite2D = $Sprite2D # Accessing Sprite2D node
 #var playback: AnimationNodeStateMachinePlayback #onready olsaydı animation_tree hazır olmadan bunu açmaya çalışıcaktı
 @export var player: Player # Globel scope'de type olarak oluşturulan player'i düşmanımıza inspectordan atayabiliyoruz world sahnesinde
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+
+
 """func _ready() -> void: 
 	animation_tree.active = true 
 	playback = animation_tree.get("parameters/StateMachine/playback") # Animation_tree deki statemachine içindeki stateler'i playback kapsıyor
@@ -37,3 +40,12 @@ func is_player_in_range() -> bool:
 		if distance_to_player < range:
 			result = true
 	return result
+
+func can_see_player() -> bool:
+	if not is_player_in_range(): return false
+	var player: = get_player()
+	ray_cast_2d.target_position = player.global_position - global_position
+	var has_los_to_player: = not ray_cast_2d.is_colliding()
+	return has_los_to_player
+	
+	
